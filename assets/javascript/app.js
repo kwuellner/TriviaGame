7d9b0c90c4b9c6edea3861b629b9cb0f9abfd599
Kyle Wuellner <rockstar81@comcast.net>
2019-07-01 23:42:29 -0500

    added time in html, button disapper on click and started questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+$('#start').on('click', function () {
+    $('#start').remove();
+})
+
+var questions = [{
+    question: "Which restaurant chain did Pam get banned from?",
+    answers: ["Chili's", "Benihana", "Applebee's", "TGIF"]
+}]
